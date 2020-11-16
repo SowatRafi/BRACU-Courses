8631969faa4bbc447617fe9a97c8632c7b9ebd01
SowatRafi <sowat.hossain.rafi@g.bracu.ac.bd>
2020-11-16 13:53:02 +0600

    Problem has been solved (Confusion)

--- CSE-Courses/Fall2020-CSE321/LABS/LAB-02/Task-01.sh
+++ CSE-Courses/Fall2020-CSE321/LABS/LAB-02/Task-01.sh
@@ -6,18 +6,25 @@ function TaxCheck() {
       local tax=$(( ($1 * 0)/100 ))
       echo "$tax"
 
-    elif (( $1 > 240000 && $1 <= 300000 ))
+    elif (( $1 > 240000 && $1-240000 <= 300000 ))
     then
-        local tax=$(( ($1 * 10)/100 ))
+        local tax=$((  ( ($1 - 240000) * 10)/100 ))
         echo "$tax"
 
-    elif (( $1 > 300000 && $1 <= 1800000 ))
+    elif (( $1-240000-300000 <= 180000 ))
     then
-        local tax=$(( ($1 * 20)/100 ))
+        local step1=$(( ( ($1 - 240000)*0)/100 ))
+        local step2=$(( (300000 * 10)/100 ))
+        local step3=$(( ( ($1 - 240000 - 300000)*20)/100 ))
+        local tax=$(( $step1 + $step2 + $step3 ))
         echo "$tax"
 
     else
-      local tax=$(( ($1 * 30)/100 ))
+      local step1=$(( ( ($1 - 240000)*0)/100 ))
+      local step2=$(( (300000 * 10)/100 ))
+      local step3=$(( (180000*20)/100 ))
+      local step4=$(( ( ($1 - 240000 - 300000 - 180000)* 30)/100 ))
+      local tax=$(( $step1 + $step2 + $step3 + $step4 ))
       echo "$tax"
     fi
 }
