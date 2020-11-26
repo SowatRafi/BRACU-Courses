c54dc05f341f749fce19e756dbc0ada6f01f34fc
SowatRafi <sowat.hossain.rafi@g.bracu.ac.bd>
2020-11-27 03:01:28 +0600

    Threads version 01

--- CSE-Courses/Fall2020-CSE321/LABS/LAB-03/src/Task_01_Tester.java
+++ CSE-Courses/Fall2020-CSE321/LABS/LAB-03/src/Task_01_Tester.java
@@ -1,9 +1,21 @@
 public class Task_01_Tester {
     public static void main(String[] args) {
-        MyThread myThread1 = new MyThread("task-01");
-        MyThread myThread2 = new MyThread("task-02");
+        Task_01 myThread1 = new Task_01();
+        Task_01 myThread2 = new Task_01();
 
-        myThread1.start();
-        myThread2.start();
+        myThread1.Step_One();
+        //System.out.println(myThread1.getName());
+//        try {
+//            myThread1.join();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+        myThread2.Step_Two();
+        //System.out.println(myThread2.getName());
+
+
+        myThread1.Step_Three();
+        //System.out.println(myThread1.getName());
     }
 }
--- CSE-Courses/Fall2020-CSE321/LABS/LAB-03/src/cse321/lab/Main.java
+++ CSE-Courses/Fall2020-CSE321/LABS/LAB-03/src/cse321/lab/Main.java
@@ -1,8 +1,34 @@
 package cse321.lab;
 
 public class Main {
-
+    // main method represents main thread
     public static void main(String[] args) {
-        System.out.println("Hello Again.");
+        // whatever we write in here will be executed by main thread
+        // Threads always execute the job in a sequence
+        // Execution Content
+
+        // Job 01
+        System.out.println("...Application started...");
+
+        // Job 02
+        MyTask task = new MyTask();
+        //task.executeTask();
+        task.start(); // -> Start shall internally run method
+
+        // Till Job 02 is not finished, blow written jobs are waiting and are not executed
+        // In case Job 02 is a long running operation, i.e. several documents are suppose to be printed
+        // In such a use case OS/JVM shall give a message that app is not responding
+        // Some sluggish behaviour in app can be observed -> App seems to hang
+
+        // Now, main MyTask are executing parallely or concurrently !!
+
+        // Job 03
+        // Some code to print the documents
+        for (int i = 1 ; i <= 10 ; i++) {
+            System.out.println("Printing Document #"+ i + " - Printer:1");
+        }
+
+        // Job 04
+        System.out.println("...Application Finished...");
     }
 }
