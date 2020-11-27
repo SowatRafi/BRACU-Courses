f34b07c5991f8da2c137acc6cc47cba4b55f5cc8
SowatRafi <sowat.hossain.rafi@g.bracu.ac.bd>
2020-11-27 11:22:49 +0600

    Fixed Task 01

--- CSE-Courses/Fall2020-CSE321/LABS/LAB-03/src/Task_01.java
+++ CSE-Courses/Fall2020-CSE321/LABS/LAB-03/src/Task_01.java
@@ -1,22 +1,2 @@
-public class Task_01 extends Thread {
-//    public Task_01(String name){
-//        super(name);
-//    }
-
-    public void Step_One() {
-        for (int i = 1; i <= 10; i++) {
-            System.out.println("This output is from " + getName() +" : " + i);
-        }
-    }
-    public void Step_Two() {
-        for (int i = 11; i <= 20; i++) {
-            System.out.println("This output is from " + getName() +" : " + i);
-        }
-    }
-
-    public void Step_Three() {
-        for (int i = 21; i <= 30; i++) {
-            System.out.println("This output is from " + getName() +" : " + i);
-        }
-    }
+package PACKAGE_NAME;public class Task_01 {
 }
