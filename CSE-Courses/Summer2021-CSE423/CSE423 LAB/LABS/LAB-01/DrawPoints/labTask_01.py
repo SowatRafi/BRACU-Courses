42b0574e81ee5ba19806c22d8327433493db1e5e
Sowat Hossain Rafi <sowat.hossain.rafi@g.bracu.ac.bd>
2021-07-10 18:25:43 +0600

    Done with Task-01 logic

---
+++ CSE-Courses/Summer2021-CSE423/CSE423 LAB/LABS/LAB-01/DrawPoints/labTask_01.py
@@ -0,0 +1,13 @@
+from OpenGL.GL import *
+import numpy as np
+
+x = np.random.randint(0, 500, 50)
+y = np.random.randint(0, 500, 50)
+
+def something():
+    glPointSize(5)
+    glBegin(GL_POINTS)
+    for i in range(0, 50):
+        glVertex2f(x[i], y[i])
+    glEnd()
+
